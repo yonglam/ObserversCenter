1dce633abeef1a20ccd71cd121e5906fd26386b2
yonglam <ngubenson@163.com>
2015-03-02 22:29:38 +0800

    Init
    Init submit

---
+++ ObserversCenter.h
@@ -0,0 +1,43 @@
+//
+//  ObserversCenter.h
+//  WeixinFoundation
+//
+//  Created by Lin Yong on 15/3/2.
+//  Copyright (c) 2015年 Lin Yong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "PLObservers.h"
+
+///
+// This class is to decouple the observers and the observered.
+//
+// Usage:
+//
+// 1. without key
+// ADD_OBSERVER(ptl, observer)
+// REMOVE_OBSERVER(ptl, observer)
+// NOTIFY_OBSERVERS(ptl, func)
+//
+// 2. with a key
+// ADD_OBSERVER_WITH_KEY(ptl, key, observer)
+// REMOVE_OBSERVER_WITH_KEY(ptl, key, observer)
+// NOTIFY_OBSERVERS_WITH_KEY(ptl, key, func)
+///
+
+@interface ObserversCenter : NSObject
+
++ (instancetype)defaultCenter;
+
+- (PLObservers *)getObservers:(Protocol *)ptl;
+- (PLObservers *)getObservers:(Protocol *)ptl forKey:(NSString *)key;
+
+@end
+
+#define ADD_OBSERVER(ptl, observer) {[[[ObserversCenter defaultCenter] getObservers:@protocol(ptl)] addObserver:observer];}
+#define REMOVE_OBSERVER(ptl, observer) {[[[ObserversCenter defaultCenter] getObservers:@protocol(ptl)] removeObserver:observer];}
+#define NOTIFY_OBSERVERS(ptl, func) {[(id<ptl>)[[ObserversCenter defaultCenter] getObservers:@protocol(ptl)] func];}
+
+#define ADD_OBSERVER_WITH_KEY(ptl, key, observer) {[[[ObserversCenter defaultCenter] getObservers:@protocol(ptl) forKey:key] addObserver:observer];}
+#define REMOVE_OBSERVER_WITH_KEY(ptl, key, observer) {[[[ObserversCenter defaultCenter] getObservers:@protocol(ptl) forKey:key] removeObserver:observer];}
+#define NOTIFY_OBSERVERS_WITH_KEY(ptl, key, func) {[(id<ptl>)[[ObserversCenter defaultCenter] getObservers:@protocol(ptl) forKey:key] func];}
---
+++ ObserversCenter.m
@@ -0,0 +1,49 @@
+//
+//  ObserversCenter.m
+//  WeixinFoundation
+//
+//  Created by Lin Yong on 15/3/2.
+//  Copyright (c) 2015年 Lin Yong. All rights reserved.
+//
+
+#import "ObserversCenter.h"
+
+@interface ObserversCenter ()
+
+@property (nonatomic) NSMutableDictionary *m_dicObservers;
+@end
+
+@implementation ObserversCenter
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _m_dicObservers = [NSMutableDictionary dictionary];
+    }
+    return self;
+}
+
++ (instancetype)defaultCenter{
+    static ObserversCenter *center = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        center = [[ObserversCenter alloc] init];
+    });
+    return center;
+}
+
+- (PLObservers *)getObservers:(Protocol *)ptl{
+    return [self getObservers:ptl forKey:nil];
+}
+
+- (PLObservers *)getObservers:(Protocol *)ptl forKey:(NSString *)key{
+    NSString *aKey = [NSString stringWithFormat:@"[%@-%@]", NSStringFromProtocol(ptl), key];
+    PLObservers *observers = [self.m_dicObservers objectForKey:aKey];
+    if (nil == observers) {
+        observers = [[PLObservers alloc] initWithObserverProtocol:ptl];
+        [self.m_dicObservers setObject:observers forKey:aKey];
+    }
+    return observers;
+}
+@end
